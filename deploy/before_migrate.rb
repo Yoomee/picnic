1c52946cb742419cd1b2438a5d089f39eb8c888f
Ian Mooney <ian@yoomee.com>
2011-04-14 15:03:49 +0100

    Removed workling from deploy hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -10,9 +10,9 @@ File.open(db_yaml_path, 'w') do |file|
   file.puts YAML::dump(db_yaml)
 end
 
-# Stop old workling client if exists
-run "cd #{current_path} && sudo RAILS_ENV=production script/workling_client stop" if File.exists?("#{current_path}/log/workling.pid")
-# Start starling
-run "cd #{release_path} && sudo starling -d -p 15151"
-# Start new workling client
-run "cd #{release_path} && sudo RAILS_ENV=production script/workling_client start"
+# # Stop old workling client if exists
+# run "cd #{current_path} && sudo RAILS_ENV=production script/workling_client stop" if File.exists?("#{current_path}/log/workling.pid")
+# # Start starling
+# run "cd #{release_path} && sudo starling -d -p 15151"
+# # Start new workling client
+# run "cd #{release_path} && sudo RAILS_ENV=production script/workling_client start"
