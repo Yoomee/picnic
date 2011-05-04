b3ffceeebb424cf33927de7f846094d21de8d0e3
Ian Mooney <ian@yoomee.com>
2011-05-04 16:58:20 +0100

    Adding shouts

--- client/config/plugins.rb
+++ client/config/plugins.rb
@@ -2,5 +2,10 @@ Tramlines.add_plugin(:enquiries)
 Tramlines.add_plugin(:events)
 Tramlines.add_plugin(:auth)
 Tramlines.add_plugin(:locations)
-
 Tramlines.add_plugin(:badges)
+Tramlines.add_plugin(:shouts)
+Tramlines.add_plugin(:walls)
+
+Tramlines.add_plugin(:notifications)
+
+Tramlines.add_plugin(:ratings)
