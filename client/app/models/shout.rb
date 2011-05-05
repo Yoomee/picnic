2d12e5f8cdcc700eff43d86ad63dca8d5028e4c7
Si Wilkins <si@yoomee.com>
2011-05-05 16:43:26 +0100

    Beginnings of browse page

--- client/app/models/shout.rb
+++ client/app/models/shout.rb
@@ -2,14 +2,23 @@ Shout.class_eval do
   
   class << self
     
-    def default_image
-      Dragonfly::App[:images].fetch('client_defaults/shout_image')
+    def default_image(image_attr = 'image')
+      Dragonfly::App[:images].fetch(default_image_location(image_attr))
+    end
+    
+    def default_image_location(image_attr = 'image')
+      "client_defaults/shout_image"
     end
     
   end
   
-  def default_image(dummy_arg = nil)
-    attachable.try(:default_image) || self.class::default_image
+  def has_image?
+    attachable.try(:has_image?)
   end
+  
+  def default_image(image_attr)
+    self.class::default_image
+  end
+  
   
 end
