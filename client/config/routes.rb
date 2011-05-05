91b9f96d57c9d8c68457ac9c4ce0889c5c2a57f5
Ian Mooney <ian@yoomee.com>
2011-05-05 12:48:40 +0100

    Changed sphinx port. Changes to members#show.

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -2,5 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   
   map.club '/club', :controller => 'club', :action => 'index'
   map.resources :venues
+  map.resources :tags, :as => "themes"
   
 end
