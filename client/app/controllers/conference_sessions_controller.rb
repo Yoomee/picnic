925600e6f28d00f3c51df186a409a3e5c33c79ff
Matt Atkins <matt@yoomee.com>
2011-05-05 16:58:00 +0100

    Programme, venues, conference sessions

---
+++ client/app/controllers/conference_sessions_controller.rb
@@ -0,0 +1,52 @@
+class ConferenceSessionsController < ApplicationController
+  
+  admin_only :create, :edit, :destroy, :index, :new, :update
+
+  before_filter :get_conference_session, :only => %w{destroy edit show update}
+  
+  def create
+    @conference_session = ConferenceSession.new params[:conference_session]
+    if @conference_session.save
+      flash[:notice] = 'Session successfully created'
+      redirect_to_waypoint
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def destroy
+    @conference_session.destroy
+    flash[:notice] = "Session successfully deleted"
+    redirect_to conference_sessions_path
+  end
+  
+  def edit
+  end
+  
+  def index
+    @conference_sessions = ConferenceSession.all(:order => 'name')
+  end
+  
+  def new
+    @conference_session = ConferenceSession.new(:starts_at =>DateTime.parse("2011-09-14T09:00"), :ends_at => DateTime.parse("2011-09-14T10:00"))
+  end
+  
+  def show
+  end
+  
+  def update
+    if @conference_session.update_attributes(params[:conference_session])
+      flash[:notice] = 'Session successfully updated'
+      redirect_to_waypoint
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  private
+  def get_conference_session
+    @conference_session = ConferenceSession.find(params[:id])
+    @section = Section.find_by_slug("picnic11")
+  end
+  
+end
--- client/app/controllers/programmes_controller.rb
+++ client/app/controllers/programmes_controller.rb
@@ -3,6 +3,13 @@ class ProgrammesController < ApplicationController
   before_filter :set_section
   
   def show
+    if Date.today > Date.parse("2011-09-14") && Date.today <= Date.parse("2011-09-16")
+      @date = Date.today
+      @time = Time.now
+    else
+      @date = Date.parse("2011-09-14")
+    end  
+    @venues = Venue.with_sessions_in_year(2011)
     
   end
   
