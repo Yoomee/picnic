d3b639ee9db8cda1294fca88defc994f65cc7b38
Si Wilkins <si@yoomee.com>
2011-05-05 17:41:00 +0100

    Added theme detail

--- client/app/controllers/tags_controller.rb
+++ client/app/controllers/tags_controller.rb
@@ -4,4 +4,9 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
   
+  def show
+    @tag = Tag.find_by_name(params[:id])
+    @stories = Shout.tagged_with(params[:id]).all.paginate(:per_page => 6, :page => params[:page])
+  end
+  
 end
