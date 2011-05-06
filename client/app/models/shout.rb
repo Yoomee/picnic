d531ac15029974d7425a7e654f2228474d7f90b4
Ian Mooney <ian@yoomee.com>
2011-05-06 13:06:58 +0100

    Added image, description to Tag. Edit/delete tags.

--- client/app/models/shout.rb
+++ client/app/models/shout.rb
@@ -1,18 +1,8 @@
 Shout.class_eval do
+
+  include TramlinesImages
   
   has_location
-  
-  class << self
-    
-    def default_image(image_attr = 'image')
-      Dragonfly::App[:images].fetch(default_image_location(image_attr))
-    end
-    
-    def default_image_location(image_attr = 'image')
-      "client_defaults/shout_image"
-    end
-    
-  end
   
   def has_image?
     attachable.try(:has_image?)
@@ -22,16 +12,12 @@ Shout.class_eval do
     attachable.try(:image)
   end
   
-  def default_image(image_attr)
-    self.class::default_image
-  end
-  
   def related_shouts
     Shout.random.limit(3)
   end
   
   def to_s
-    title
+    title || ""
   end
   
 end
