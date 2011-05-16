37cf31436b15f0f93ab5b12243cd1bbaeb6d70ee
Si Wilkins <si@yoomee.com>
2011-05-16 17:29:52 +0100

    Changes to member

--- client/app/models/member.rb
+++ client/app/models/member.rb
@@ -2,4 +2,14 @@ Member.class_eval do
   
   has_location
   
+  validates_presence_of :country
+  
+  def country
+    location.country
+  end
+  
+  def country=(val)
+    location.country = val
+  end
+  
 end
