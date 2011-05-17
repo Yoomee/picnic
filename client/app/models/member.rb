03503bb7ceb3835fdf5850c04817697513eb4eb1
Matt Atkins <matt@yoomee.com>
2011-05-17 17:35:27 +0100

    Accepts nested for urls

--- client/app/models/member.rb
+++ client/app/models/member.rb
@@ -8,6 +8,8 @@ Member.class_eval do
   validates_length_of :what_i_bring, :maximum => WHAT_I_BRING_MAX_LENGTH, :on => :update, :allow_blank => true
   validates_presence_of :what_i_bring, :on => :update
   
+  accepts_nested_attributes_for :urls
+  
   # validates_presence_of :country, :unless => Proc.new {|member| !member.new_record? || member.twitter_connected? || member.linked_in_connected? || member.facebook_connected?}
   
   def country
