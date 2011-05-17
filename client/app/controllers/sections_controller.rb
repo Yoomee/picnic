68c1399dbbcf679014f804fe45f7e1986099ca1e
Rich Wells <rich@yoomee.com>
2011-05-17 17:24:00 +0100

    Made page order change for news articles

--- client/app/controllers/sections_controller.rb
+++ client/app/controllers/sections_controller.rb
@@ -4,8 +4,8 @@ SectionsController.class_eval do
     case @section.view
     when 'latest_stories', 'news_view'
       @pages_sections = @section.pages.published.latest + @section.children
-      @pages_sections.extend(SectionsController::SortByWeightAndLatest)
-      @pages_sections = @pages_sections.sort_by_weight_and_latest.paginate(
+      @pages_sections.extend(SectionsController::SortByWeightAndPublished)
+      @pages_sections = @pages_sections.sort_by_weight_and_published.paginate(
         :page => params[:page],
         :per_page => (@section.view == 'news_view' ? 7 : (APP_CONFIG[:latest_stories_items_per_page] || 6))
       )
@@ -16,6 +16,24 @@ SectionsController.class_eval do
       # Otherwise use show view
       @pages = @section.pages.published.weighted.paginate(:page => params[:page], :per_page => (APP_CONFIG[:section_pages_items_per_page] || 10))
     end
+  end
+  
+  
+  
+  module SortByWeightAndPublished
+
+    def compare_weight_and_published(item_a, item_b)
+      weight_comp = item_a.weight <=> item_b.weight
+      return weight_comp unless weight_comp.zero?
+      item_b.publish_on <=> item_a.publish_on
+    end
+
+    def sort_by_weight_and_published
+      sort do |item_a, item_b|
+        compare_weight_and_published(item_a, item_b)
+      end
+    end
+
   end
   
 end
