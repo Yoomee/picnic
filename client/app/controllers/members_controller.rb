d69d15477dc581b4dba46d39e405c0a2915d4f6d
Matt Atkins <matt@yoomee.com>
2011-05-17 15:56:39 +0100

    What i bring length fix

--- client/app/controllers/members_controller.rb
+++ client/app/controllers/members_controller.rb
@@ -4,21 +4,21 @@ MembersController.class_eval do
 
   skip_before_filter :check_what_i_bring, :only => %w{update what_i_bring}
 
-  def create
-    puts "IN CREATE"
-    @member = Member.new(params[:member])
-    if @member.save
-      if params[:in_popup]
-        render(:text => "<script type='text/javascript'>window.close()</script>")
-      else
-        flash[:notice] = "Your account has been created. Welcome to #{APP_CONFIG['site_name']}."
-        session[:logged_in_member_id] = @member.id if @logged_in_member.nil?
-        redirect_to @member
-      end
-    else
-      render :action => 'new'
-    end
-  end
+  # def create
+  #   puts "IN CREATE"
+  #   @member = Member.new(params[:member])
+  #   if @member.save
+  #     if params[:in_popup]
+  #       render(:text => "<script type='text/javascript'>window.close()</script>")
+  #     else
+  #       flash[:notice] = "Your account has been created. Welcome to #{APP_CONFIG['site_name']}."
+  #       session[:logged_in_member_id] = @member.id if @logged_in_member.nil?
+  #       redirect_to @member
+  #     end
+  #   else
+  #     render :action => 'new'
+  #   end
+  # end
 
   def new_with_redirect
     if logged_in_member
--- client/app/models/member.rb
+++ client/app/models/member.rb
@@ -5,7 +5,7 @@ Member.class_eval do
   has_location  
   has_many :urls, :as => :attachable
   
-  validates_length_of :what_i_bring, :maximum => WHAT_I_BRING_MAX_LENGTH
+  validates_length_of :what_i_bring, :maximum => WHAT_I_BRING_MAX_LENGTH, :on => :update, :allow_blank => true
   validates_presence_of :what_i_bring, :on => :update
   
   # validates_presence_of :country, :unless => Proc.new {|member| !member.new_record? || member.twitter_connected? || member.linked_in_connected? || member.facebook_connected?}
