027269682d573f2f63aa85aef9f1edcf888581a8
Si Wilkins <si@yoomee.com>
2011-05-24 12:24:15 +0100

    Fixing more link with filtering on member profile

--- client/app/helpers/shouts_helper.rb
+++ client/app/helpers/shouts_helper.rb
@@ -20,6 +20,24 @@ ShoutsHelper.module_eval do
     end
   end
 
+  def older_shouts_link(name, url_options = {})
+    out = link_to_remote(name, :url => older_shouts_link_url(url_options), :method => :get, :html => {:id => "older_shouts_link"}, :loading => "$('#older_shouts_link').hide();$('#older_shouts_loader').show()", :complete => "$('#older_shouts_link').show();$('#older_shouts_loader').hide()")
+    out << image_tag('ajax_loader_squares.gif', :id => 'older_shouts_loader', :style => 'display:none')
+  end
+
+  def older_shouts_link_url(url_options = {})
+    url_options.reverse_merge!(:page => ((params[:page] || 1).to_i + 1), :per_page => (params[:per_page] || 10), :named_scope => params[:named_scope])
+    if parent = url_options.delete(:parent)
+      if parent.is_a?(Member)
+        url_options.merge(:id => parent.id)
+        return member_older_shouts_path(url_options)
+      else
+        url_options.merge!(:parent_type => parent.class, :parent_id => parent.id)
+      end
+    end
+    older_shouts_path(url_options)
+  end
+  
   def render_shout_with_extra_options(shout, options = {})
     options.reverse_merge!(:show_member => true)
     render_shout_without_extra_options(shout, options)
--- client/config/routes.rb
+++ client/config/routes.rb
@@ -1,25 +1,26 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.club '/club', :controller => 'club', :action => 'index'
+  map.resource :programme
+
+  map.resources :conference_sessions
+  map.resources :tags, :as => "themes", :collection => {:autocomplete => :get}
   map.resources :urls
   map.resources :venues
-  map.resources :conference_sessions
-  map.resource :programme
-  map.resources :tags, :as => "themes", :collection => {:autocomplete => :get}
-  
+
+  map.all_time_leaderboard "leaderboard/all_time", :controller => "leaderboard", :action => "all_time"
+  map.browse '/browse', :controller => 'browse'
+  map.club '/club', :controller => 'club', :action => 'index'
+  map.connections "/connections", :controller => 'club', :action => 'connections'
+  map.correspondents '/correspondents', :controller => 'correspondents'
+  map.leaderboard "leaderboard", :controller => "leaderboard", :action => "index"
+  map.member_older_shouts "/member_older_shouts/:id", :controller => 'members', :action => 'older_shouts'
+  map.news_network '/newsnetwork', :controller => 'news_network'
+  map.themes '/themes', :controller => 'tags'
+  map.what_i_bring "/what_i_bring", :controller => 'members', :action => 'what_i_bring'
+
   map.connect '/delegate/receive', :controller => 'delegate_receiver', :action => 'receive'
   map.connect '/delegate/test', :controller => 'delegate_receiver', :action => 'test'
-  
-  map.leaderboard "leaderboard", :controller => "leaderboard", :action => "index"
-  map.all_time_leaderboard "leaderboard/all_time", :controller => "leaderboard", :action => "all_time"
-  
-  map.correspondents '/correspondents', :controller => 'correspondents'
-  map.browse '/browse', :controller => 'browse'
-  map.themes '/themes', :controller => 'tags'
-  map.news_network '/newsnetwork', :controller => 'news_network'
-  map.what_i_bring "/what_i_bring", :controller => 'members', :action => 'what_i_bring'
-  map.connections "/connections", :controller => 'club', :action => 'connections'
-  map.leaderboard "/leaderboard", :controller => 'club', :action => 'leaderboard'
   map.connect "tags/autocomplete", :controller => "tags", :action => "autocomplete"
   
 end
+
