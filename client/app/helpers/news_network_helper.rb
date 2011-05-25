ef60ccf295360aa02dbf209f6b8d2dcf58923d20
Ian Mooney <ian@yoomee.com>
2011-05-25 13:03:21 +0100

    Changes to tags#show.

--- client/app/helpers/news_network_helper.rb
+++ client/app/helpers/news_network_helper.rb
@@ -1,7 +1,7 @@
 module NewsNetworkHelper
   
   def in_news_network?
-    controller_name.in?(%w{browse correspondents news_network tags})
+    controller_name.in?(%w{browse correspondents news_network})
   end
   
 end
