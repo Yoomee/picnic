b06b0dc4ff55458c381b25f53e6c0a49d9a75771
Si Wilkins <si@yoomee.com>
2011-05-26 16:21:31 +0100

    Fixed problem with tags banner

--- client/app/helpers/club_helper.rb
+++ client/app/helpers/club_helper.rb
@@ -1,13 +1,13 @@
 module ClubHelper
   
   def tag_li_attributes(tag, tag_array)
-    returning out = {:id => "connection_tag_#{tag}"} do
+    returning out = {:id => "connection_tag_#{tag.to_s.fully_underscore}"} do
       out[:class] = 'active_tag' if tag == tag_array.first
     end
   end
   
   def tags_bottom_attributes(tag, tag_array)
-    returning out = {:id => "tags_bottom_#{tag}"} do
+    returning out = {:id => "tags_bottom_#{tag.to_s.fully_underscore}"} do
       out[:style] = 'display:none' unless tag == tag_array.first
     end
   end
