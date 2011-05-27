073bc493a5016d89aba49fcac6d99ab84b8ba14c
Si Wilkins <si@yoomee.com>
2011-05-27 17:52:56 +0100

    Fixed spam problem

--- client/app/controllers/pages_controller.rb
+++ client/app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ PagesController.class_eval do
   
   def show_with_contact_form
     show_without_contact_form
-    @enquiry = Enquiry.new(:form_name => 'Beta', :member => logged_in_member) if @page.slug == 'beta'
-    spamify(@enquiry)
+    if @page.slug == 'beta'
+      @enquiry = Enquiry.new(:form_name => 'Beta', :member => logged_in_member) if @page.slug == 'beta'
+      spamify(@enquiry)
+    end
   end
   alias_method_chain :show, :contact_form
   
