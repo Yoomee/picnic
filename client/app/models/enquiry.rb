ceb262ba6a03e5cf2d6f8c2bd70a471c2c32f45c
Si Wilkins <si@yoomee.com>
2011-05-27 17:37:39 +0100

    Change to picnicer

--- client/app/models/enquiry.rb
+++ client/app/models/enquiry.rb
@@ -1,5 +1,10 @@
 Enquiry.class_eval do
   
-  acts_as_textcaptcha({'api_key' => '6t5vlvftcusc0gwgokgks08wog18qgtm'})
+  acts_as_textcaptcha({
+      'api_key' => '6t5vlvftcusc0gwgokgks08wog18qgtm',
+      'questions' => [
+        {'question' => 'Which of these would be useful for a Picnic? A lightbulb, a hammer, a sandwich', 'answers' => 'a sandwich, sandwich'}
+      ]
+  })
   
 end
