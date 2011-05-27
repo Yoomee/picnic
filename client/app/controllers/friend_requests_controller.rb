7a65d9f09d7375ca18bb90b650d22c0a803df501
Ian Mooney <ian@yoomee.com>
2011-05-27 17:30:57 +0100

    Styles friend list.

--- client/app/controllers/friend_requests_controller.rb
+++ client/app/controllers/friend_requests_controller.rb
@@ -6,6 +6,7 @@ FriendRequestsController.class_eval do
       if logged_in_as?(@friend_request.member_target) && @friend_request.accept!
         page["#friend_request_#{@friend_request.member_id}_link"].replace_html("You are now sitting on #{@friend_request.member.forename}'s blanket.")
         page["#friend_list_#{@friend_request.member_id}"].replace(render("members/friend_list", :member => @friend_request.member(true)))
+        page << refresh_fb_dom        
       end
     end
   end
@@ -13,6 +14,7 @@ FriendRequestsController.class_eval do
   def deal_with_create
     render :update do |page|
       page[".friends_list"].replace_html(render("members/friends", :member => @member_target))
+      page << refresh_fb_dom
     end
   end
   
