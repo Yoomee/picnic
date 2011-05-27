bad85b1e603b902f18ecee1265c442c38a8156fc
Matt Atkins <matt@yoomee.com>
2011-05-27 14:40:59 +0100

    Beginnings of verify link

--- client/app/controllers/conference_delegates_controller.rb
+++ client/app/controllers/conference_delegates_controller.rb
@@ -5,6 +5,27 @@ class ConferenceDelegatesController < ApplicationController
   def index
     @conference_delegates = ConferenceDelegate.latest.paginate(:page => params[:page], :per_page => 20)
   end
+  
+  def verify
+    conference_delegate = ConferenceDelegate.find_by_signature(params[:signature])
+    if conference_delegate && conference_delegate.member.nil?
+      if logged_in_member
+        if logged_in_member.conference_delegate.nil?
+          conference_delegate.update_attribute(:member, logged_in_member)
+          flash[:notice] = "Thanks for verifying your PICNIC 11 booking."
+        else
+          flash[:notice] = "You've already verified your PICNIC 11 booking."
+        end
+        redirect_to club_path
+      else
+        @member = Member.new(:conference_delegate => conference_delegate)
+        render :template => 'members/new'
+      end
+    else
+      flash[:notice] = "This PICNIC 11 booking has already been verified."
+      redirect_to club_path
+    end
+  end
 
   
 end
--- client/app/controllers/delegate_receiver_controller.rb
+++ client/app/controllers/delegate_receiver_controller.rb
@@ -2,7 +2,8 @@ class DelegateReceiverController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   admin_only :test
-  open_action :receive  
+  open_action :receive
+  
   def receive
     conf_delegate = ConferenceDelegate.create_from_params!(params)
     DELEGATE_LOGGER.info(" (from #{request.remote_ip})\n\n#{params.to_yaml}\nSIGNATURE: #{params["SIGNATURE"]}\nEXPECTED:  #{conf_delegate.try(:expected_signature)}\n\n#{request.env.inspect}")
