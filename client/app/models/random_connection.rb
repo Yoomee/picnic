187cd277b0e583cdca8601ffcf2e133c1d029454
Si Wilkins <si@yoomee.com>
2011-06-03 12:07:53 +0100

    Fixed random connection to not show friends

--- client/app/models/random_connection.rb
+++ client/app/models/random_connection.rb
@@ -7,7 +7,7 @@ class RandomConnection < Connection
     private
     def args_for_initialize(member, options = {})
       reasons = RandomConnection::REASONS.dup
-      Member.not_including(member).random.limit(options[:limit]).map {|member| [member, reasons.remove_one_at_random!]}
+      Member.not_including(member).without_friend_request_with(member).random.limit(options[:limit]).map {|member| [member, reasons.remove_one_at_random!]}
     end
     
   end
