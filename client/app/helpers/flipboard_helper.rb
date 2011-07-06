fe4e475d90e2047467b45f0864f48b784a994611
Matt Atkins <matt@yoomee.com>
2011-07-06 15:33:03 +0100

    More flips

---
+++ client/app/helpers/flipboard_helper.rb
@@ -0,0 +1,17 @@
+module FlipboardHelper
+  
+  def render_flip_item(flipitem, template)
+    if flipitem.is_a?(String)
+      flip_partial = flipitem
+    elsif flipitem.respond_to?(:flip_partial)
+      flip_partial = flipitem.flip_partial
+    elsif flipitem.is_a?(Struct::Tweet)
+      flip_partial = "tweet"
+    else
+      flip_partial = flipitem.class.to_s.underscore
+    end
+    render("flipboard/flips/#{flip_partial}", :flipitem => flipitem, :x => template[0], :y => template[1])
+  end
+  
+  
+end
