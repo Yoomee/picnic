248e5db2dc69d51789ba70328ce6943f41c4b1e1
Matt Atkins <matt@yoomee.com>
2011-07-12 11:49:34 +0100

    Friends in order. Flipboard h6 font.

--- client/app/models/friend_request.rb
+++ client/app/models/friend_request.rb
@@ -1,7 +1,7 @@
 FriendRequest.class_eval do
 
   def accept!
-    if !is_accepted? && update_attribute(:is_accepted, true)
+    if !is_accepted? && update_attributes(:is_accepted => true, :accepted_at => Time.now)
       member.notifications.create(:text => "<a href='/members/#{member_id_target}'>#{member_target}</a> is now sitting on your blanket.", :from_member => member_target)
     end
     notification.destroy if notification && is_accepted?
