c6bc3f0a6b49e1d8c22c4d91a6eacb397926091e
Si Wilkins <si@yoomee.com>
2011-07-15 16:49:50 +0100

    Added mobile fix

---
+++ client/app/helpers/mobile_helper.rb
@@ -0,0 +1,14 @@
+module MobileHelper
+  
+  def is_mobile_device?
+    request.user_agent.to_s.downcase =~ Regexp.new(MobileHelper::MOBILE_USER_AGENTS)
+  end
+  
+end
+MobileHelper::MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                      'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                      'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                      'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                      'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+                      'mobile'
+
