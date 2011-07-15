d4d5c50f26cd275c6ff9d4ab2f00088a0a64174f
Matt Atkins <matt@yoomee.com>
2011-07-15 11:21:21 +0100

    Search fix.

--- client/app/controllers/search_controller.rb
+++ client/app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 SearchController.class_eval do
   
   def create
-    options = {:match_mode => params[:match_mode], :autocomplete => params[:autocomplete], :page => params[:page], :per_page => SearchController::PER_PAGE, :models => %w{Member Page Section Shout WallPost}}
+    options = {:match_mode => params[:match_mode], :autocomplete => params[:autocomplete], :page => params[:page], :per_page => SearchController::PER_PAGE}
     @search_filter = params[:search_filter]
-    @search = Search.new params[:search], options
+    @search = Search.new params[:search].merge(:models => %w{Member Page Section Shout}), options
     if request.xhr?
       partial_view_path = params[:results_view_path] ? "#{params[:results_view_path]}/" : ""
       return render(:partial => "#{partial_view_path}ajax_search_results", :locals => {:search => @search})
