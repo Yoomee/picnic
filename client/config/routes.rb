981f0ba63b82d9f7111be319cc20d3a4d637843a
Ian Mooney <ian@yoomee.com>
2011-08-11 17:22:10 +0100

    Changed conferences#programme to program in routes

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -4,7 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :conference_sessions, :only => [:create, :edit, :destroy, :show, :update], :member => {:duplicate => :get, :attend => :post, :unattend => :delete}
   map.resources :conferences, :member => {:order_venues => :get, :update_venue_weights => :post} do |conf|
     conf.resources :conference_sessions, :as => 'sessions', :only => [:show, :new]
-    conf.resource :programme, :only => [:show]
+    conf.resource :programme, :as => "program", :only => [:show]
     conf.resource :venues, :only => [:show, :new]
   end
   map.resources :front_covers, :member => {:activate => :post, :deactivate => :post}
