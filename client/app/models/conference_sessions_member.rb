3be719f4162c96f841f3310238f1f9ac1711da0d
Matt Atkins <matt@yoomee.com>
2011-08-24 11:18:08 +0100

    Don't require presence of conference_session for conference_sessions_member as was preventing adding speakers to new conference sessions.

--- client/app/models/conference_sessions_member.rb
+++ client/app/models/conference_sessions_member.rb
@@ -1,6 +1,5 @@
 class ConferenceSessionsMember < ActiveRecord::Base  
   belongs_to :member
-  belongs_to :conference_session
-  
-  validates_presence_of :member, :conference_session 
+  belongs_to :conference_session  
+  validates_presence_of :member
 end
